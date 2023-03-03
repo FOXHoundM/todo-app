fbde3c7130f5578c62bd304999f195776cac84f0
Mikael Gulmamedov <4216623@gmail.com>
2023-03-03 20:22:42 +0300

    todoItem & todoList css

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -9,9 +9,9 @@ const TodoItem: React.FC<ITodoItem> = ({id, title, complete}) => {
   return (
     <div className='TodoItem'>
 
-      <input className='TodoItem__input' type='checkbox' checked={complete}/>
-      {title}
-      {/*<button className="TodoItem__button"/>*/}
+      <input className='TodoItem__input' type='radio' checked={complete}/>
+      <h1 className="TodoItem__title">{title}</h1>
+      <button className="TodoItem__button"/>
 
     </div>
   );
